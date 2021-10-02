b2ae616d6bf102d91fbbd84cf7ee69839c0bd227
Jesus <jesus.mallo.paya@gmail.com>
2021-10-02 22:11:54 +0200

    feat header and footer

---
+++ components/Showcase.js
@@ -0,0 +1,12 @@
+import styles from "@/styles/Showcase.module.css";
+
+import React from "react";
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome To the Party!</h1>
+      <h2>Find the hottest DJ events</h2>
+    </div>
+  );
+}
